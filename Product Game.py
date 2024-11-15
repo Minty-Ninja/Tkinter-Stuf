855c8c05a9b14d461644f35b1208c4454f1dbe45
Minty-Ninja <idhant.kumar10@gmail.com>
2024-11-15 21:19:23 +0530

    Update Product Game.py

--- Product Game.py
+++ Product Game.py
@@ -20,3 +20,45 @@ En= Entry(root)
 
 
 root.mainloop()
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+from tkinter import *
+import tkinter as tk
+import random
+from tkinter import messagebox
+
+
+root=Tk()
+root.title('Product Game')
+
+def make(): 
+    num1= random.randint(1,10)
+    num2 = random.randint(1,10)
+    return num1, num2, num1*num2
+    
+num1,num2,result= make()
+
+#Generating UI
+label = Label(root, text='Guess the product of {} and ?'.format(num1))
+label.pack()
+
+
+En= Entry(root)
+En.pack()
+
+b1= Button(root, text='Check?')
+b1.pack()
+
+
+def NewGame():
+    global num1, num2, result
+    num1, num2, result= make()
+    messagebox.showinfo("A random number is {}".format(num2))
+    En.delete(0,tk.END)
+
+new_game=Button(root, text='New Game', command=NewGame)
+new_game.pack()
+NewGame()
+
+root.mainloop()
