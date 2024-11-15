d3e458f5fc8d44d93ffeff5557ac1bb1eb7794c5
Minty-Ninja <idhant.kumar10@gmail.com>
2024-11-15 20:52:05 +0530

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -1,12 +1,22 @@
 from tkinter import *
 from tkinter.ttk import *
 from time import strftime
+import random
 
+def Randomm():
+    x= random.randint(0,255)
+    y= random.randint(0,255)
+    z= random.randint(0,255)
+    return f"#{x:02x}{y:02x}{z:02x}"
 
 root=Tk()
 root.title('Clock')
-label= Label(root, font=('Pacifico', 40, 'bold'), background = 'orange', foreground='green')
+label= Label(root, font=('Pacifico', 40, 'bold'), background = Randomm(), foreground=Randomm())
 label.pack(anchor='center')
+
+
+
+
 
 def time():
     string=strftime('%H:%M:%S %p') 
