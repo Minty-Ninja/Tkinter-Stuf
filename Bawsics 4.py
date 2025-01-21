b1f1daf70cc1b72ba0f3a565f2095391f3426078
Minty-Ninja <idhant.kumar10@gmail.com>
2025-01-21 22:25:41 +0530

    Update Bawsics 4.py

--- Bawsics 4.py
+++ Bawsics 4.py
@@ -1,10 +1,10 @@
 import cv2
 
-x = cv2.imread("Whiteboard.jpeg")
+x = cv2.imread("Whiteboard.jpg")
 
 #Drawing a line
 sp = (0,0)
-ep = (25,25)
+ep = (500,500)
 
 col = (0, 0, 0)
 
@@ -14,3 +14,35 @@ Acte = cv2.line(x, sp, ep, col, thc)
 cv2.imshow("NewLine", Acte)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+#Drawing a Rectangle on the Image
+
+x = cv2.imread("Whiteboard.jpg")
+
+sp = (250,250)
+ep = (500,300)
+
+col = (0, 0, 0)
+
+thc = 5
+y = cv2.rectangle(x, sp, ep, col, thc)
+
+cv2.imshow("Rectangle", y)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+sp = (250,250)
+ep = (500,300)
+
+col = (0, 0, 0)
+
+thc = -1 #Colour fill
+y = cv2.rectangle(x, sp, ep, col, thc)
+
+cv2.imshow("Rectangle", y)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
