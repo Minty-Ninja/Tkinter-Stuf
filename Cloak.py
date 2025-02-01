f6663ebc982480f430f82f31929b5bbdb92364bb
Minty-Ninja <idhant.kumar10@gmail.com>
2025-02-01 13:27:27 +0530

    Update Cloak.py

--- Cloak.py
+++ Cloak.py
@@ -2,3 +2,22 @@ import cv2
 import numpy as np
 import time
 print(cv2.__version__)
+
+--------------------------------------------------------------------------------------------------
+import cv2
+import numpy as np
+import time
+
+
+print(cv2.__version__)
+
+
+rv = cv2.VideoCapture("video.mp4")
+time.sleep(1)
+
+
+
+counter = 0
+bg = 0
+
+#Capturing the bg- Used for masking
